ecb41e9bc7d5ade6ec40a3507e71c8c7715266a2
Jiasi Tan <jiasit@andrew.cmu.edu>
2018-10-26 14:56:48 -0400

    Cart Javascript - new content added
    Added functions of clearning cart; price total calculation; add, subtract, delete.

--- homework_6/js/cart.js
+++ homework_6/js/cart.js
@@ -3,4 +3,35 @@ $(document).ready(function(){
     var glazing = null;
     var price = null;
     var count = null;
+
+    $("#dialog").hide();
+    $("#clear-cart").click(function(){
+        clearCart();
+        displayCart();
+    });
+    $(".add-to-cart").click(function(event){
+        event.preventDefault();
+        var name = $(this).attr("data-name");
+        var price = Number($(this).attr("data-price"));
+        addItemToCart(name,price,1);
+        displayCart();
+    });
+    
+    $("#show-cart").on("click",".delete-item",function(event){
+        var name = $(this).attr("data-name");
+        removeItemFromCartAll(name);
+        displayCart();
+    });
+    $("#show-cart").on("click",".subtract-item",function(event){
+        var name = $(this).attr("data-name");
+        removeItemFromCart(name);
+        displayCart();
+    });
+    $("#show-cart").on("click",".add-item",function(event){
+        var name = $(this).attr("data-name");
+        addItemToCart(name,0,1);
+        displayCart();
+    });
+
+
 });
