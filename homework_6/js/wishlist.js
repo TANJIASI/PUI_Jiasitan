5d332ee60e53cd3c104aa272606a42ccb9444381
Jiasi Tan <jiasit@andrew.cmu.edu>
2018-10-30 18:29:16 -0400

    Update homework_6/js/wishlist.js
    working in progress

---
+++ homework_6/js/wishlist.js
@@ -0,0 +1,226 @@
+$(document).ready(function(){
+    var name = null;
+    var glazing = null;
+    var price = null;
+    var count = null;
+    var pack = null;
+    var shoppingCart = {};
+    shoppingCart.cart = [];
+
+    $("#dialog").hide();
+    $("#clear-cart").click(function(){
+        clearCart();
+        displayCart();
+    });
+    $(".add-to-cart").click(function(event){
+        event.preventDefault();
+        var name = $(this).attr("data-name");
+        var price = Number($(this).attr("data-price"));
+        addItemToCart(name,price,1);
+        displayCart();
+    });
+
+    $("#show-cart").on("click",".delete-item",function(event){
+        var name = $(this).attr("data-name");
+
+        var cart_item = $(this).parent();
+        console.log(cart_item.children(".name").text());
+        var item_name = cart_item.children(".name").text();
+        var item_glazing = cart_item.children(".glazing").text();
+        var item_pack = cart_item.children(".pack").text();
+        item_pack = parseInt(item_pack);
+
+
+        removeItemFromCartAll(item_name, item_glazing, item_pack);
+        displayCart();
+    });
+    $("#show-cart").on("click",".subtract-item",function(event){
+        // var item_name = $(this).attr("data-name");
+        var cart_item = $(this).parent();
+        console.log(cart_item.children(".name").text());
+        var item_name = cart_item.children(".name").text();
+        var item_glazing = cart_item.children(".glazing").text();
+        var item_pack = cart_item.children(".pack").text();
+        item_pack = parseInt(item_pack);
+        removeItemFromCart(item_name, item_glazing, item_pack);
+        displayCart();
+    });
+    $("#show-cart").on("click",".add-item",function(event){
+        // console.log($(this).parent());
+        var cart_item = $(this).parent();
+        console.log(cart_item.children(".name").text());
+        var item_name = cart_item.children(".name").text();
+        var item_glazing = cart_item.children(".glazing").text();
+        var item_price = cart_item.children(".price").text();
+        var item_pack = cart_item.children(".pack").text();
+
+        item_price = parseFloat(item_price);
+        // item_pack = parseInt(item_pack);
+        console.log(item_pack);
+
+        // var name = $(this).attr("data-name");
+        addItemToCart(item_name, item_glazing, item_price, item_pack, 1);
+        displayCart();
+    });
+
+    // get type
+    $(".glazing").click(function(){
+        glazing = $(this).attr("data-name");
+        if(glazing == "None"){
+            glazing = "no glazing";
+        }
+        console.log("glazing", glazing);
+        $(".glazing").css({"border":"2px solid #d6d6d6", "color":"black", "background-color":"white"});
+        $(this).css({"border":"2px solid #d6d6d6", "color":"white", "background-color":"gray"});
+    });
+    // get count
+    $(".pack").click(function(){
+        pack = $(this).attr("data-pack");
+        pack = parseInt(pack);
+        console.log("pack", pack);
+        $(".pack").css({"border":"2px solid #d6d6d6", "color":"black", "background-color":"white"});
+        $(this).css({"border":"2px solid #d6d6d6", "color":"white", "background-color":"gray"});
+    });
+    // get price
+    $("#addtocart").click(function (event) {
+        name = "Walnut Roll";
+        price = 4.99;
+        count = 1;
+        // store to local storage
+        event.preventDefault();
+        if(count==null){
+            alert("Please select number of items needed!");
+            $(".count").css({"border":"2px solid red", "color":"red", "background-color":"white"});
+        }else{
+            var getResponse = function(){
+                console.log("Get Response Done");
+                console.log(name);
+                console.log(glazing);
+                console.log(price);
+                console.log(pack);
+                console.log(count);
+                addItemToCart(name, glazing, price, pack, count);
+                $( this ).dialog( "close" );
+
+            };
+            var cancel = function(){
+            };
+            var value="You selected " + pack +" "+glazing + name + "s. The items will be added to your cart.";
+            $("#dialog").text(value);
+            $("#dialog").dialog({buttons:{OK:getResponse, Cancel:cancel}});
+        }
+    });
+
+    loadCart();
+    displayCart();
+});
+
+var Item = function(name, glazing, price, pack, count) {
+    this.name = name;
+    this.glazing = glazing;
+    this.price = price;
+    this.pack = pack;
+    this.count = 1;
+};
+
+function displayCart(){
+    var cartArray = listCart();
+    var output = "";
+    for (var i in cartArray){
+        output += "<div class='cart-item'>"
+            +"<img src='./images/walnut.jpg' style='width:100px;'>"
+            +"<p class='name'>"+cartArray[i].name+"</p><br>"
+            +"<p class='glazing'>"+cartArray[i].glazing+"</p><br>"
+            +"<p>(</p><p class='pack'>"+cartArray[i].pack+"</p><p>-pack)</p><br>"
+            +"<button class='subtract-item' data-name='"
+            +cartArray[i].name+"'>-</button>"
+            +" "+cartArray[i].count
+            +"<button class='add-item' data-name='"
+            +cartArray[i].name+"'>+</button>"
+            +" x <p class='price'>"+cartArray[i].price+"</p>"
+            +" = "+cartArray[i].total
+            +"<button class='delete-item' data-name='"
+            +cartArray[i].name+"'> Delete </button>"
+            +"</div>";
+    }
+    $("#show-cart").html(output);
+    $("#total-cart").html(totalItemCost());
+}
+function listCart(){
+    var cartCopy = [];
+    for (var i in cart){
+        var item = cart[i];
+        var itemCopy = {};
+        for (var j in item){
+            itemCopy[j] = item [j];
+        }
+        itemCopy.total = (item.price * item.count * item.pack).toFixed(2);
+        cartCopy.push(itemCopy);
+    }
+    return cartCopy;
+}
+function saveCart(){
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+function loadCart(){
+    cart = JSON.parse(localStorage.getItem("shoppingCart"));
+}
+function removeItemFromCartAll(name, glazing, pack){
+    for (var i in cart){
+        if (cart[i].name === name && cart[i].glazing===glazing && cart[i].pack===pack){
+            cart.splice(i,1);
+            break;
+        }
+    }
+    saveCart();
+}
+function clearCart(){
+    cart = [];
+    saveCart();
+}
+function countCart(){
+    var totalCount = 0;
+    for (var i in cart) {
+        totalCount += cart[i].count;
+    }
+    return totalCount;
+}
+function totalItemCost(){
+    var totalCost = 0;
+    for (var i in cart){
+        totalCost += cart[i].price * cart[i].count *cart[i].pack;
+    }
+    return totalCost.toFixed(2);
+}
+
+function addItemToCart (name, glazing, price, pack, count) {
+    //if same item has been selected
+    if (cart == undefined){
+        cart = [];
+    }
+
+    for (var i in cart){
+        if (cart[i].name === name && cart[i].glazing === glazing &&cart[i].pack == pack){
+            cart[i].count += count;
+            saveCart();
+            return;
+        }
+    }
+    //if not
+    var item = new Item (name, glazing, price, pack, count);
+    cart.push(item);
+    saveCart();
+}
+function removeItemFromCart(name, glazing, pack){
+    for (var i in cart){
+        if (cart[i].name === name && cart[i].glazing === glazing && cart[i].pack==pack){
+            console.log("matched");
+            cart[i].count -- ;
+            if (cart[i].count === 0) {
+                cart.splice(i,1);
+            }
+            break;
+        }
+    }
+    saveCart();
+}
